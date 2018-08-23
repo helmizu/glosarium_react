d6f5b1a0079e416cc5f3cdaa9b78362c039d4214
Muhammad Ilham Fajar <fajarmuhammadilham@gmail.com>
2018-08-23 16:27:09 +0700

    fixed components

--- src/components/Glosarium/Glosarium.js
+++ src/components/Glosarium/Glosarium.js
@@ -2,32 +2,59 @@ import React, { Component } from 'react';
 import SideBar from './SideBar';
 import Search from './Search';
 import Content from './Content';
+import { isMobile } from 'react-device-detect';
 
 class Glosarium extends Component {
-  render() {
-    return (
-      <div>
-      <div id="glosarium">
-        <div className="container container-padding">
-            <div className="row">
 
-                <SideBar />
+    contentLoad = () => {
+        if (isMobile) {
+            return <div id="glosarium">
+                <div className="container container-padding">
+                    <div className="row">
 
-                <div className="col-md-9 col-xs-12 col-sm-9">
-                    <div className="glosarium-wrapper">
-                        
                         <Search />
-                        
-                        <Content />
 
+                        <SideBar />
+
+                        <div className="col-md-9 col-xs-12 col-sm-9">
+                            <div className="glosarium-wrapper">
+
+                                <Content />
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-      </div>
-    )
-  }
+        } else {
+            return <div id="glosarium">
+                <div className="container container-padding">
+                    <div className="row">
+
+                        <SideBar />
+
+                        <div className="col-md-9 col-xs-12 col-sm-9">
+                            <div className="glosarium-wrapper">
+
+                                <Search />
+
+                                <Content />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.contentLoad()}
+            </div>
+        )
+    }
 }
 
 export default Glosarium
